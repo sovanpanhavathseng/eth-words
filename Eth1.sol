86aa051f2cf7be2d5fa0fef9dbcdb399cf25a16c
Seng Sovanpanhavath <114840775+sovanpanhavathseng@users.noreply.github.com>
2023-03-10 09:42:12 +0700

    Create Eth1.sol

---
+++ Eth1.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.8.0;
+
+contract MyContract {
+    string message;
+
+    function setMessage(string memory newMessage) public {
+        message = newMessage;
+    }
+
+    function getMessage() public view returns (string memory) {
+        return message;
+    }
+}
