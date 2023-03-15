643203fcf220a8206f6cc026d10976ede9d71e90
Seng Sovanpanhavath <114840775+sovanpanhavathseng@users.noreply.github.com>
2023-03-15 17:02:22 +0700

    Create Mainnet.sol

---
+++ Mainnet.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.8.0;
+
+contract MyContract {
+  uint256 public myVariable;
+
+  constructor(uint256 initialValue) {
+    myVariable = initialValue;
+  }
+
+  function updateVariable(uint256 newValue) public {
+    myVariable = newValue;
+  }
+}
