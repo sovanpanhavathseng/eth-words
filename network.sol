66bddf7e4499eef029a419e27c4cdcf1a82dc70c
Seng Sovanpanhavath <114840775+sovanpanhavathseng@users.noreply.github.com>
2023-03-16 21:49:19 +0700

    Create network.sol

---
+++ network.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.8.0;
+
+contract MyContract {
+    mapping(address => uint256) public balances;
+    
+    function deposit() public payable {
+        balances[msg.sender] += msg.value;
+    }
+    
+    function withdraw(uint256 amount) public {
+        require(balances[msg.sender] >= amount, "Insufficient balance");
+        balances[msg.sender] -= amount;
+        payable(msg.sender).transfer(amount);
+    }
+}
