574acf617d40b8e0cd914ec3a50acc19deadb231
Seng Sovanpanhavath <114840775+sovanpanhavathseng@users.noreply.github.com>
2023-03-17 10:59:58 +0700

    Create optimistic_rollup.sol

---
+++ optimistic_rollup.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.8.0;
+
+contract RollupContract {
+    mapping (address => uint) public balances;
+
+    function deposit() public payable {
+        balances[msg.sender] += msg.value;
+    }
+
+    function withdraw(uint amount) public {
+        require(balances[msg.sender] >= amount, "Insufficient balance");
+        balances[msg.sender] -= amount;
+        payable(msg.sender).transfer(amount);
+    }
+
+    function transfer(address recipient, uint amount) public {
+        require(balances[msg.sender] >= amount, "Insufficient balance");
+        balances[msg.sender] -= amount;
+        balances[recipient] += amount;
+    }
+}
