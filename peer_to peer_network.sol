190dc2961b8263fe3f379fb47e3996c9c4ac8698
Seng Sovanpanhavath <114840775+sovanpanhavathseng@users.noreply.github.com>
2023-03-17 11:11:35 +0700

    Create peer_to peer_network.sol

---
+++ peer_to peer_network.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.8.0;
+
+contract TokenExchange {
+    mapping (address => uint256) public balances;
+
+    function deposit() public payable {
+        balances[msg.sender] += msg.value;
+    }
+
+    function withdraw(uint256 _amount) public {
+        require(balances[msg.sender] >= _amount, "Insufficient balance");
+        balances[msg.sender] -= _amount;
+        payable(msg.sender).transfer(_amount);
+    }
+
+    function transfer(address _to, uint256 _amount) public {
+        require(balances[msg.sender] >= _amount, "Insufficient balance");
+        balances[msg.sender] -= _amount;
+        balances[_to] += _amount;
+    }
+}
