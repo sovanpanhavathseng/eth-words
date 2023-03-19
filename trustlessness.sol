7b9ea152f31b1c2de9b48348fd2d0ec84e398f6e
Seng Sovanpanhavath <114840775+sovanpanhavathseng@users.noreply.github.com>
2023-03-19 09:40:19 +0700

    Create trustlessness.sol

---
+++ trustlessness.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.8.0;
+
+contract MyTrustlessContract {
+    
+    mapping(address => uint256) public balances;
+
+    function deposit() public payable {
+        balances[msg.sender] += msg.value;
+    }
+
+    function withdraw(uint256 _amount) public {
+        require(balances[msg.sender] >= _amount, "Insufficient balance");
+        balances[msg.sender] -= _amount;
+        payable(msg.sender).transfer(_amount);
+    }
+}
