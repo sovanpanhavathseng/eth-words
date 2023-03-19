90c25e42bfc4492b33293677b44dc2d0929b972f
Seng Sovanpanhavath <114840775+sovanpanhavathseng@users.noreply.github.com>
2023-03-19 09:08:34 +0700

    Update terminal_total_difficulty_(TTD).sol

--- terminal_total_difficulty_(TTD).sol
+++ terminal_total_difficulty_(TTD).sol
@@ -7,3 +7,11 @@ contract TotalDifficulty {
         totalDifficulty = block.difficulty;
     }
 }
+
+pragma solidity ^0.8.0;
+
+contract TotalDifficulty {
+    function getBlockDifficulty(uint256 blockNumber) public view returns (uint256) {
+        return block.difficulty;
+    }
+}
